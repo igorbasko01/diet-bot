01f9c84ca4e538ff5f8b628330bc5e91d1177d63
igorbasko <igor@Igors-MacBook-Pro.local>
2018-07-10 07:37:39 +0300

    Move the bot token retrieval logic

--- main.py
+++ main.py
@@ -16,16 +16,9 @@ from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 import webapp2
 
-class BotToken(ndb.Model):
-    token = ndb.StringProperty();
-    
-queriedToken = ndb.Key('BotToken', 'dietbottoken').get()
-tokenMessage = 'No token was found !'
-if queriedToken is not None:
-    tokenMessage = 'Token Found !'
-logging.info(tokenMessage)
+import bottoken
 
-TOKEN = queriedToken.token
+TOKEN = bottoken.get_token()
 
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
 
