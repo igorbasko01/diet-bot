3449feadd73cb0f65266795523cac0e9ab982036
igorbasko01 <igorbasko@gmail.com>
2018-10-22 22:47:11 +0300

    register commands and execute them

--- commander.py
+++ commander.py
@@ -1,7 +1,17 @@
+import logging
+
 class Commander:
     def __init__(self):
         self.commands = {}
 
-    def add_command(self, cmd, func):
+    def register_command(self, cmd, func):
+        #TODO: Don't allow to register existing key.
         self.commands[cmd] = func
+
+    def execute(self, cmd):
+        logging.info('Going to execute the following command: ' + cmd)
+        try:
+            self.commands[cmd]()
+        except KeyError, e:
+            logging.info('Couldn\'t find the following registered command: ' % str(cmd))
         
