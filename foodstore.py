2e47828ea9f4fe662120a64ebd56e43b58850231
igorbasko01 <igorbasko@gmail.com>
2019-01-19 21:59:32 +0200

    remove all foodstore functions, and relevant tests

--- foodstore.py
+++ foodstore.py
@@ -4,36 +4,20 @@ from google.appengine.ext import ndb
 
 import myutils
 
-# Stores a list of different foods that the user configured.
-# Used as an index, to know which foods exists in the system.
-class FoodsListStore(ndb.Model):
-    listOfNames = ndb.StringProperty()
-
+# Stores the calories of specific foods.
+# The user_id is stored not only as part of a key,
+# it is also as part of the entity, so it could be searched.
+# Another thing is that deault food calories are stored under
+# user_id = 0. But the key is only the food's emoji.
 class FoodCalorieValues(ndb.Model):
     user_id = ndb.IntegerProperty()     # 0 Is default
     calories = ndb.IntegerProperty()
 
 def registerFoodStoreCommands(commander):
-    commander.register_command('/showfoods', showListOfFoods)
-    commander.register_command('/addfood', addFood)
     commander.register_command('/add_food_default', add_food_default)
     commander.register_command('/add_food', add_food_user)
     commander.register_command('/show_food', show_food)
     commander.register_command('/show_foods', show_foods)
-
-def getListOfFoods():
-    return ndb.Key('FoodsListStore', 'FoodsList').get()
-
-def storeListOfFoods(foodsToStore):
-    foodsList = FoodsListStore(key=ndb.Key('FoodsListStore', 'FoodsList'),listOfNames=foodsToStore)
-    foodsList.put()
-
-def showListOfFoods(request_body):
-    queryFoodList = getListOfFoods()
-    foodList = 'No foods !'
-    if queryFoodList is not None:
-        foodList = queryFoodList.listOfNames
-    return foodList
 
 def add_food_default(request_body, params):
     return add_food(request_body, params, is_user=False)
@@ -99,33 +83,3 @@ def show_foods(request_body):
     result = '\n'.join(all_foods)
     
     return result.decode('utf-8')
-
-def addFood(request_body, params):
-    if len(params) != 2:
-        logging.info('Params: {}'.format(params))
-        return 'Didn\'t fully understand. Should be like: /addfood walnut 30'
-
-    foodName = params[0]
-    calories = params[1]
-    if not myutils.is_number(calories) or int(calories) < 1:
-        return 'Invalid calories ! should be a number greater than 0.'
-
-    result_msg = ''
-
-    # Get list of food names.
-    queryFoodList = getListOfFoods()
-    foodsListArray = []
-    if queryFoodList is not None:
-        foodsListArray = queryFoodList.listOfNames.split(',')
-    logging.info('Got food list: ' + str(foodsListArray))
-
-    # add the new food only if its not already in the list.
-    if foodName not in foodsListArray:
-        foodsListArray.append(foodName)
-        foodsListString = ",".join(foodsListArray)
-        storeListOfFoods(foodsListString)
-        result_msg = 'Food ' + foodName + ' added, calories: ' + str(calories) + '.'
-    else:
-        result_msg = 'Food ' + foodName + ' already exists...'
-
-    return result_msg
--- tests/test_foodstore.py
+++ tests/test_foodstore.py
@@ -7,69 +7,6 @@ import myutils
 class TestFoodstore(unittest.TestCase):
     nosegae_datastore_v3 = True
     
-    def test_show_list_of_foods(self):
-        foodstore.storeListOfFoods("apple,hummus")
-        listOfFoods = foodstore.showListOfFoods({})
-        print listOfFoods
-        assert listOfFoods == "apple,hummus"
-
-    def test_register_command(self):
-        cmndr = Commander()
-        foodstore.registerFoodStoreCommands(cmndr)
-        foodstore.storeListOfFoods("fries")
-        listOfFoods = cmndr.execute("/showfoods", {})
-        assert listOfFoods == "fries"
-
-
-    def test_add_food_new(self):
-        cmndr = Commander()
-        foodstore.registerFoodStoreCommands(cmndr)
-        result = cmndr.execute('/addfood', {}, ['h','30'])
-        assert result == 'Food h added, calories: 30.'
-
-
-    def test_add_food_no_params(self):
-        cmndr = Commander()
-        foodstore.registerFoodStoreCommands(cmndr)
-        result = cmndr.execute('/addfood', {})
-        assert result == 'Didn\'t fully understand. Should be like: /addfood walnut 30'
-
-
-    def test_add_food_less_params(self):
-        cmndr = Commander()
-        foodstore.registerFoodStoreCommands(cmndr)
-        result = cmndr.execute('/addfood', 'hello')
-        assert result == 'Didn\'t fully understand. Should be like: /addfood walnut 30'
-
-
-    def test_add_food_more_params(self):
-        cmndr = Commander()
-        foodstore.registerFoodStoreCommands(cmndr)
-        result = cmndr.execute('/addfood', ['hello','dfasd', 'asdfa'])
-        assert result == 'Didn\'t fully understand. Should be like: /addfood walnut 30'
-
-
-    def test_add_food_exists(self):
-        cmndr = Commander()
-        foodstore.registerFoodStoreCommands(cmndr)
-        cmndr.execute('/addfood', {}, ['h','200'])
-        result = cmndr.execute('/addfood', {}, ['h','100'])
-        assert result == 'Food h already exists...'
-
-
-    def test_add_food_calories_not_number(self):
-        cmndr = Commander()
-        foodstore.registerFoodStoreCommands(cmndr)
-        result = cmndr.execute('/addfood', {}, ['h','twenty'])
-        assert result == 'Invalid calories ! should be a number greater than 0.'
-
-
-    def test_add_food_emoji(self):
-        cmndr = Commander()
-        foodstore.registerFoodStoreCommands(cmndr)
-        result = cmndr.execute('/addfood', {}, ['\U0001f33d','100'])
-        assert result == 'Food \\U0001f33d added, calories: 100.'
-
     def test_add_default_food(self):
         cmndr = Commander()
         foodstore.registerFoodStoreCommands(cmndr)
