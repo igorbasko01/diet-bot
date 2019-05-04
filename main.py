833ad97bd1db8bdc222c64135a8828b038b37f02
igorbasko01 <igorbasko@gmail.com>
2019-05-04 20:37:57 +0300

    Add help message function

--- main.py
+++ main.py
@@ -108,6 +108,23 @@ class WebhookHandler(webapp2.RequestHandler):
             logging.info('send response:')
             logging.info(resp)
 
+        def help_msg():
+            return '''
+            Hi {},
+            Thanks for choosing me for your calories tracking.
+            To log a food that you've eaten, just send \u1F355 or any
+            food that you prefer.
+            To track coffee intake, just send \u2615, it also will
+            track the calories of the coffee.
+            To log water intake, just send \u1F376.
+            And of course all the emojis can be combined in one message.
+            To list the caloric value of all available foods to track
+            send the /show_foods command.
+            To change the caloric value of a food or add another food
+            for tracking use the /add_food command.
+            There are some more commands, feel free to explore.
+            '''.format(name)
+
         cmd, params = myutils.split_text(text)
         params_encoded = [ x.encode('utf-8') for x in params]
 
