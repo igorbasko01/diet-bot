dee55b22dead6aa14bed00fef8da14d49c903b5d
igorbasko01 <igorbasko@gmail.com>
2019-05-10 22:01:14 +0300

    Add a help command

--- userstore.py
+++ userstore.py
@@ -10,6 +10,7 @@ class UserStore(ndb.Model):
 def register_user_commands(commander):
     commander.register_command('/set_max_calories', set_max_calories)
     commander.register_command('/show_max_calories', reply_max_calories)
+    commander.register_command('/help', help_msg)
 
 
 def set_max_calories(request_body, params):
@@ -38,4 +39,24 @@ def reply_max_calories(request_body):
         return '{}, sorry but you didn\'t set max calories.'.format(name)
     else:
         return '{}, your max calories is {}'.format(name, user_obj.max_calories)
+
+    
+def help_msg(request_body):
+    name = myutils.extract_user_first_name(request_body)
+
+    return u'''
+            Hi {},
+            Thanks for choosing me for your calories tracking.
+            To log a food that you've eaten, just send \ud83c\udf55 or any
+            food that you prefer.
+            To track coffee intake, just send \u2615, it also will
+            track the calories of the coffee.
+            To log water intake, just send \ud83c\udf76.
+            And of course all the emojis can be combined in one message.
+            To list the caloric value of all available foods to track
+            send the /show_foods command.
+            To change the caloric value of a food or add another food
+            for tracking use the /add_food command.
+            There are some more commands, feel free to explore.
+            '''.format(name)
     
